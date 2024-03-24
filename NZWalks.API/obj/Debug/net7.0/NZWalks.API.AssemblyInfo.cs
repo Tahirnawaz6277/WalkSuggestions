bad427a68600ce12782734240ebf11454bd06ccf
Tahirnawaz <tahirnawazwebpro@gmail.com>
2024-03-24 21:33:04 +0100

    apply previous stash changes

--- NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b33e827084c8ac0ada016e6c0d709f29ffbf5c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
